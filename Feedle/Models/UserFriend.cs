625d71ab41f79298d75334f76a43ba9193805238
Marton Pentek <pentek.marci@gmail.com>
2020-12-13 19:02:55 +0100

    19.02

---
+++ Feedle/Models/UserFriend.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FeedleDataTier.Models
+{
+    public class UserFriend
+    {
+        [Key]
+        [JsonPropertyName("userFriendId")]
+        public int UserFriendId { get; set; }
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("friendId")]
+        public int FriendId { get; set; }
+    }
+}
